c4b8fa2bb8938bc8c03c82a5dc0fbff6b7ed370b
ZIWEI CHEN <kchen325@student.ubc.ca>
2024-01-10 18:37:55 -0800

    finish

--- src/MathDistOps/pnorm.py
+++ src/MathDistOps/pnorm.py
@@ -7,15 +7,18 @@ def pnorm(x, mean=0, std_dev=1, plot_graph=True):
     Cumulative Distribution Function (CDF) of the normal distribution.
 
     Parameters:
+    --------
     - x (float): The value at which to evaluate the CDF.
     - mean (float, optional): The mean (average) of the normal distribution. Default is 0.
     - std_dev (float, optional): The standard deviation of the normal distribution. Default is 1.
     - plot_graph (bool, optional): Whether to plot the CDF graph. Default is True.
 
     Returns:
+    --------
     float: The cumulative probability up to the given value `x`.
 
     Formula:
+    --------
     F(x; μ, σ) = (1 / 2) * [1 + erf((x - μ) / (σ * sqrt(2)))]
 
     The CDF describes the probability that a random variable in a normal distribution
@@ -23,10 +26,12 @@ def pnorm(x, mean=0, std_dev=1, plot_graph=True):
     and standard deviation (`σ`), determining the center and spread of the distribution.
 
     Example:
+    --------
     >>> pnorm(1, mean=0, std_dev=1)
     0.8413447460685429
 
     Graph:
+    --------
     - The blue curve represents the normal distribution CDF for the given parameters.
     - The vertical red line corresponds to the specified value `x`.
 
@@ -46,16 +51,10 @@ def pnorm(x, mean=0, std_dev=1, plot_graph=True):
     plt.legend()
     plt.show()
     '''
-    
+
     Warning:
     The CDF is a continuous probability distribution function, and the area under
     the curve represents the probability of the variable falling below a certain value.
 
-
-
-
-
-
-
-
     """
+    pass
