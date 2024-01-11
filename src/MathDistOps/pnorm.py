e7b6c9104914409954b56028437ae4c1e6470402
ZIWEI CHEN <kchen325@student.ubc.ca>
2024-01-10 18:11:33 -0800

    parameter

--- src/MathDistOps/pnorm.py
+++ src/MathDistOps/pnorm.py
@@ -1,3 +1,16 @@
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+
+def pnorm(x, mean=0, std_dev=1, plot_graph=True):
+    """
+    Cumulative Distribution Function (CDF) of the normal distribution.
+
+    Parameters:
+    - x (float): The value at which to evaluate the CDF.
+    - mean (float, optional): The mean (average) of the normal distribution. Default is 0.
+    - std_dev (float, optional): The standard deviation of the normal distribution. Default is 1.
+    - plot_graph (bool, optional): Whether to plot the CDF graph. Default is True.
+
+    
+    """
