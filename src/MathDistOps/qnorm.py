ff16a2e9564ed5f3138467d11269d7a347cbc9a0
ZIWEI CHEN <kchen325@student.ubc.ca>
2024-01-10 18:49:20 -0800

    title

--- src/MathDistOps/qnorm.py
+++ src/MathDistOps/qnorm.py
@@ -2,3 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.special import erfinv
 
+def qnorm(p, mean=0, std_dev=1, plot_graph=True):
+    """
+    Quantile (Inverse Cumulative Distribution Function) of the normal distribution.
+
+    
+    """
