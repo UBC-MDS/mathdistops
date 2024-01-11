cbdf4ec366752682147539ae91b9b65844591176
ZIWEI CHEN <kchen325@student.ubc.ca>
2024-01-10 18:50:06 -0800

    parameter

--- src/MathDistOps/qnorm.py
+++ src/MathDistOps/qnorm.py
@@ -6,5 +6,13 @@ def qnorm(p, mean=0, std_dev=1, plot_graph=True):
     """
     Quantile (Inverse Cumulative Distribution Function) of the normal distribution.
 
-    
+    Parameters
+    ----------
+    - p (float): The probability for which to find the quantile.
+    - mean (float, optional): The mean (average) of the normal distribution. Default is 0.
+    - std_dev (float, optional): The standard deviation of the normal distribution. Default is 1.
+    - plot_graph (bool, optional): Whether to plot the quantile graph. Default is True.
+
+
+
     """
