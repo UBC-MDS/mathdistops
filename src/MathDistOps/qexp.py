ad3cc330dabb1cb31a4461c21abb26e5bd7dc81b
sivakornchong <sivakornchong@gmail.com>
2024-01-11 18:13:34 -0800

    Synchronise the name for plotting

--- src/MathDistOps/qexp.py
+++ src/MathDistOps/qexp.py
@@ -1,4 +1,4 @@
-def qexp(p, rate, output_image=False):
+def qexp(p, rate, plot_graph=False):
     """
     Calculates the quantile corresponding to given cumulative probability in an exponential distribution and plots the corresponding distribution.
 
@@ -13,7 +13,7 @@ def qexp(p, rate, output_image=False):
         Must be between 0 and 1, inclusive.
     rate : float
         The rate parameter (`lambda`) of the exponential distribution. Must be a positive number.
-    output_image : bool, optional
+    plot_graph : bool, optional
         If True, generates and returns a plot of the exponential distribution with
         the quantile highlighted for the given cumulative probability. Default is False.
 
@@ -26,7 +26,7 @@ def qexp(p, rate, output_image=False):
 
     Examples
     --------
-    >>> qexp(0.5, rate=1, output_image=True)
+    >>> qexp(0.5, rate=1, plot_graph=True)
     (0.6931471805599453, [Matplotlib Figure Object])
 
     Notes
