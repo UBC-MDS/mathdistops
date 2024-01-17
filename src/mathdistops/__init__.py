bd792a712dce2c12aa59bb0338a9db78d5403d06
Sandra Gross <sandra-starnberg@web.de>
2024-01-16 19:00:06 -0800

    Changed init file

--- src/mathdistops/__init__.py
+++ src/mathdistops/__init__.py
@@ -1,3 +1,8 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("mathdistops")
+
+from mathdistops.pnorm import pnorm
+from mathdistops.qnorm import qnorm
+from mathdistops.qexp import qexp
+from mathdistops.pexp import pexp
