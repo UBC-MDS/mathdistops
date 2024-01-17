3d25df788f17edfdabc846fb26480dd10202a9d0
KUN YA <kunya@live.cn>
2024-01-17 15:55:57 -0800

    feat: update pexp test with result check

--- tests/test_pexp.py
+++ tests/test_pexp.py
@@ -1,7 +1,8 @@
-from mathdistops.pnorm import pnorm
+from mathdistops.pexp import pexp
 import pytest
 import pandas as pd
 import altair as alt
+import math
 
 def test_output_datatype_pexp():
     """
@@ -16,15 +17,16 @@ def test_df_results_pexp():
     """
     Tests whether the pexp function calculates the cumulative probability value correctly
     """
-    results = pexp(1, graph=False)
+    results = pexp(1, rate=1, graph=False)
+    expected_probability = 1 - math.exp(-1)
     assert results.shape == (1, 2), "Expected DataFrame shape to be (1, 2)"
     assert results['Quantile'].iloc[0] == 1, "Expected correct quantile value"
-    assert 0 <= results['Cumulative probability'].iloc[0] <= 1, "Expected cumulative probability to be between 0 and 1"
+    assert results['Cumulative probability'].iloc[0] == pytest.approx(expected_probability), "Expected cumulative probability did not match for q=1, rate=1"
 
     results_df = pexp(2, rate=2, graph=False)
+    expected_probability_df = 1 - math.exp(-2 * 2)
     assert results_df['Quantile'].iloc[0] == 2, "Expected correct quantile value"
-    cumulative_probability = results_df['Cumulative probability'].iloc[0]
-    assert 0 <= cumulative_probability <= 1, "Expected cumulative probability to be between 0 and 1"
+    assert results_df['Cumulative probability'].iloc[0] == pytest.approx(expected_probability_df), "Expected cumulative probability did not match for q=2, rate=2"
 
 def test_missing_input_pexp():
     """
@@ -40,8 +42,9 @@ def test_nonsensical_input_pexp():
     """
     with pytest.raises(ValueError) as custom_string:
         results = pexp(3, rate=-1, graph=False)
-    assert str(custom_string.value) == "Rate cannot be zero or negative.", "Expected ValueError for negative rate"
+    assert str(custom_string.value) == "Rate cannot be zero or negative."
 
     with pytest.raises(TypeError) as custom_string:
         results = pexp('hi', rate=2, graph=False)
-    assert str(custom_string.value), "Input parameters must be numerical.", "Expected TypeError for non-numerical input"
+    assert str(custom_string.value), "Input parameters must be numerical."
+
