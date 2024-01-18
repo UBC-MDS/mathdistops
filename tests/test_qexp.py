ca072aa22c4ea9b5f238d0969c946179319c5f49
ZIWEI CHEN <kchen325@student.ubc.ca>
2024-01-17 19:35:06 -0800

    test_quantile_calculation

--- tests/test_qexp.py
+++ tests/test_qexp.py
@@ -2,3 +2,11 @@ from mathdistops.qexp import qexp
 import pytest
 import altair as alt
 import numpy as np
+
+def test_quantile_calculation():
+    """
+    Tests whether function calculates quantile correctly.
+    """
+    # Known values for specific cases
+    assert qexp(0.5, 1, False)[0] == -np.log(0.5)
+    assert qexp(0.75, 2, False)[0] == -np.log(0.25) / 2
